dc1a7b1e73c1192c92a4a5f6699c71c69c789e1f
andres.chapo <andres.chapo@bue.edu.ar>
2021-07-17 16:34:25 -0300

    Grid cass complete

--- minesweeper/MinesweeperGrid.py
+++ minesweeper/MinesweeperGrid.py
@@ -3,12 +3,13 @@ import random
 VACIO = 0
 MINE = 'x'
 
+
 class MinesweeperGrid:
     def __init__(self, sizes, mines_cuantities):
         self.sizes = sizes
         self.mines_cuantities = mines_cuantities
         self.grid = self.new_grid()
-        #        self.set_values_in_grid()
+        self.add_points_to_grid()
         self.swept = set()
 
     def new_grid(self):
@@ -36,7 +37,7 @@ class MinesweeperGrid:
         for y in range(self.sizes):
             field.append([])
             for x in range(self.sizes):
-                if (y,x) in self.swept:
+                if (y, x) in self.swept:
                     field[y].append(str(self.grid[y][x]))
                 else:
                     field[y].append('.')
@@ -53,26 +54,43 @@ class MinesweeperGrid:
         elif self.grid[row][column] > 0:
             return True
 
-        desde = max(0, row - 1)
-        hasta = min(self.sizes - 1, row + 1) + 1
-        for r in range(desde, hasta):
-            desde_columna = max(0, column - 1)
-            hasta_columna = min(self.sizes - 1, column + 1) + 1
-            for c in range(desde_columna, hasta_columna):
-                if (r, c) in self.swept:
-                    continue
-                self.sweep(r, c)
-
+        from_row = max(0, row - 1)
+        until_row = min(self.sizes - 1, row + 1) + 1
+        for y in range(from_row, until_row):
+            from_column = max(0, column - 1)
+            until_column = min(self.sizes - 1, column + 1) + 1
+            for x in range(from_column, until_column):
+                if (y, x) in self.swept: continue
+                self.sweep(y, x)
         return True
 
-grid = MinesweeperGrid(10, 10)
-grid.grid[1][1] = 1
-grid.grid[1][2] = 2
-grid.grid[1][3] = 3
-print(grid.grid)
-grid.sweep(1,1)
-grid.sweep(1,2)
-grid.sweep(1,3)
-grid.sweep(1,4)
+    def add_points_to_grid(self):
+        rang = range(self.sizes)
+        for y in rang:
+            for x in rang:
+                if self.grid[y][x] == MINE: continue
+                else: self.grid[y][x] = self.get_near_mines(y,x)
 
+    def get_near_mines(self, row, column):
+        num_near_mines = 0
+        from_row = max(0, row - 1)
+        until_row = min(self.sizes - 1, row + 1) + 1
+        for y in range(from_row, until_row):
+            from_column = max(0, column - 1)
+            until_column = min(self.sizes - 1, column + 1) + 1
+            for x in range(from_column, until_column):
+                if y == row and x == column: continue
+                if self.grid[y][x] == MINE: num_near_mines += 1
+
+        return num_near_mines
+
+grid = MinesweeperGrid(10, 10)
+print(grid.grid)
+grid.sweep(1, 1)
+grid.sweep(1, 2)
+grid.sweep(1, 3)
+grid.sweep(1, 4)
 print(grid)
+#print(grid.get_near_mines(9,0))
+#lista = range(0,10)
+#print(lista)
